01b79c0d60fcfda32b41e52fdbd1f6ba522976a0
Yegoroff <AlexYegoroff+github@gmail.com>
2011-01-31 09:53:10 +0800

    Web Repository

---
+++ NConfig/Implementation/ConfigurationRepositoryWeb.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Web;
+
+namespace NConfig
+{
+
+    /// <summary>
+    /// Provides web specific configuration file location.
+    /// </summary>
+    internal class ConfigurationRepositoryWeb : ConfigurationRepository
+    {
+
+        protected override string ToAbsolutePath(string path)
+        {
+            if (!VirtualPathUtility.IsAbsolute(path))
+                path = VirtualPathUtility.ToAppRelative("\\" + path);
+
+            return System.Web.Hosting.HostingEnvironment.MapPath(path);
+        }
+
+    }
+}

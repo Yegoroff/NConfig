c149c43c8efb8e1b0592f6cf69b9432ced9876fd
Yegoroff <AlexYegoroff+github@gmail.com>
2011-11-16 00:12:24 +0200

    Added NuGet Gallery publishing.

---
+++ NuGet Publish.bat
@@ -0,0 +1,16 @@
+@set NUGET="Lib\NuGet.CommandLine.1.5.21005.9019\tools\NuGet.exe"
+
+@echo ==========================
+@echo NuGet package publishing.
+@%NUGET% Push NuGet\NConfig.1.0.6.nupkg
+@if not errorlevel 0 goto error
+
+@echo NConfig publishing sucessfull.
+@goto end
+
+:error
+@echo Error occured during NConfig publishing.
+@pause
+
+:end
+@pause

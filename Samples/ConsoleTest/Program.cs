f87fa76ad23f1ca480d385f88166cb8edf1b6f9b
Yegoroff <AlexYegoroff+github@gmail.com>
2011-11-15 16:38:41 +0200

    Improved Samples\ConsoleTest sample

--- Samples/ConsoleTest/Program.cs
+++ Samples/ConsoleTest/Program.cs
@@ -1,44 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using NConfig;
 using System.Configuration;
-using System.Configuration.Internal;
-using System.Reflection;
-using System.Globalization;
 
 namespace ConsoleTest
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Start");
+            Console.WriteLine("Simple NConfig Sample. \r\n");
+
+            // Setup NConfigurator to use Custom.config file from Config subfolder.
+            NConfigurator.UsingFile(@"Config\Custom.config").SetAsSystemDefault();
 
 
-            NConfigurator.UsingFile("Config\\HostMap.config").PromoteToDefault();
+            var testSection = NConfigurator.Default.GetSection<TestConfigSection>();
+
+            var configManagerTestSection = ConfigurationManager.GetSection("TestConfigSection") as TestConfigSection;
+
+            var namedTestSection = NConfigurator.UsingFile(@"Config\Custom.config").GetSection<TestConfigSection>("NamedSection");
 
 
-            TestConfig tc =  NConfigurator.Default.GetSection<TestConfig>();
-            Console.WriteLine(tc.TestValue);
+            Console.WriteLine("NConfig Default : {0} \r\n".F(testSection.TestValue));
+            Console.WriteLine("ConfigurationManager : {0} \r\n".F(configManagerTestSection.TestValue));
+            Console.WriteLine("NConfig named section : {0} \r\n".F(namedTestSection.TestValue));
 
-            object sect = ConfigurationManager.GetSection("TestConfig");
+            Console.WriteLine("");
+            Console.WriteLine("MERGED APP SETTINGS : ");
+            foreach(var key in ConfigurationManager.AppSettings.AllKeys)
+            {
+                Console.WriteLine(key + " : " +  ConfigurationManager.AppSettings[key]);
+            }
 
-            NConfigurator.UsingFile("Config\\HostMap.config").GetSection<TestConfig>("testConfig");
+            Console.WriteLine("");
+            Console.WriteLine("MERGED CONNECTION STRINGS : ");
+            foreach (ConnectionStringSettings connectionString in ConfigurationManager.ConnectionStrings)
+            {
+                Console.WriteLine(connectionString.Name + " : " + connectionString.ConnectionString);
+            }
 
-
-            Console.WriteLine(NConfigurator.UsingFile("Config\\HostMap.config").ConnectionStrings.Count);
-
-
-
-            TestConfig c = NConfigurator.UsingFile("Config\\HostMap.config").GetSection<TestConfig>();
-            Console.WriteLine(c.TestValue);
-
-            Console.WriteLine("Finish");
             Console.ReadKey();
         }
     }
 
+    static class StringExtensions
+    {
+        public static string F(this string format, params object[] args)
+        {
+            return string.Format(format, args);
+        }
+    }
 
 }

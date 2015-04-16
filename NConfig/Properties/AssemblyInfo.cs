b237c36047cd9cea1f5c7304e9e20d0465f12116
ayrov <ayroff@gmail.com>
2015-04-16 20:23:10 +0300

    version 1.0.18

--- NConfig/Properties/AssemblyInfo.cs
+++ NConfig/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NConfig")]
 [assembly: AssemblyDescription("Multifile host-based configuration for .Net")]
 [assembly: AssemblyProduct("NConfig")]
-[assembly: AssemblyCopyright("Copyright © Alexander Yegorov 2014")]
+[assembly: AssemblyCopyright("Copyright © Alexander Yegorov 2015")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("d9ab0c68-93c0-4881-94cd-8297025f3ad9")]
 
-[assembly: AssemblyVersion("1.0.17.0")]
-[assembly: AssemblyFileVersion("1.0.17.0")]
+[assembly: AssemblyVersion("1.0.18.0")]
+[assembly: AssemblyFileVersion("1.0.18.0")]
 [assembly: InternalsVisibleTo("NConfig.Tests")]
